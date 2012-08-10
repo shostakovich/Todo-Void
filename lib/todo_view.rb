1bbf1c0a1f29e77f1c4cd91d26e7934df965f7ec
Robert Curth <robert@rocu.de>
2012-08-10 16:59:14 +0200

    Refactoring of todo_view

--- lib/todo_view.rb
+++ lib/todo_view.rb
@@ -6,16 +6,34 @@ class TodoView
   end
 
   def render
-    output = if @todo.status == :finished
-      "#{@todo.id} #{@todo.description}".foreground(:black)
-    elsif @todo.status == :started
-      "#{@todo.id.foreground(:green)} #{@todo.description}"
+    output = "#{formated_id} #{@todo.description}"
+    output += tags
+    mark_if_finished(output) 
+  end
+
+  private
+  def formated_id
+    if @todo.status == :started
+      @todo.id.foreground(:green)
+    elsif @todo.status == :pending
+      @todo.id.foreground(:yellow)
     else
-      "#{@todo.id.foreground(:yellow)} #{@todo.description}"
+      @todo.id
     end
-    unless @todo.tags.empty?
-      output += " (#{@todo.tags.join(', ')})".foreground(:green)
+  end
+
+  def tags
+    return "" if @todo.tags.empty?
+    tags = " (#{@todo.tags.join(', ')})"
+    tags = tags.foreground(:green) unless @todo.status == :finished
+    tags
+  end
+
+  def mark_if_finished(output)
+    if @todo.status == :finished
+      output.foreground(:black)
+    else
+      output
     end
-    output
   end
 end
